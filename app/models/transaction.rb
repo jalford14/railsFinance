3a1041d56fe95f3387c4356f0d850c7330610ed5
Jimmy Alford <jalford14@liberty.edu>
2018-07-13 13:56:24 -0700

    Can edit and destroy entries.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ApplicationRecord
+    validates :category, presence: true
+    validates :business, presence: true
+    validates :amount, presence: true,
+            numericality: { greater_than: 0 }
 end
