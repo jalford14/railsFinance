b85c18f8a5d521defdd0b99d006ded3925ba327e
Jimmy Alford <jalford14@liberty.edu>
2018-07-18 13:48:04 -0700

    Changed form to not be hardcoded

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,10 @@ class Transaction < ApplicationRecord
     validates :business, presence: true
     validates :amount, presence: true,
             numericality: { greater_than: 0 }
+    before_save :downcase_fields
+
+   def downcase_fields
+      self.category.downcase!
+      self.business.downcase!
+   end
 end
