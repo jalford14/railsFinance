36b2f7a6b5fcc15e70e2d909c62702a2ddaba0db
Jimmy Alford <jalford14@liberty.edu>
2018-07-20 10:21:15 -0700

    added controller definitions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,30 @@
 class CategoriesController < ApplicationController
     def index
-        @transaction = Transaction.all
+        @category = Transaction.all
     end
 
     def show
         @category = Transaction.all
+    end
+
+    def new
+        @category = Transaction.new
+    end
+
+    def create
+        @category = Transaction.new(transaction_params)
+        
+        if @category.save
+            redirect_to @category
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        @category = Transaction.find(params[:id])
+        @category.destroy
+        
+        redirect_to categories_path
     end
 end
