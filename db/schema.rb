669434da1fb1a97a310d2e6f7d94008bef85281a
Jimmy Alford <jalford14@liberty.edu>
2018-07-20 17:28:20 -0700

    Seperating table with category as pk. (not a working branch yet)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_031134) do
+ActiveRecord::Schema.define(version: 2018_07_21_002408) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "transactions", force: :cascade do |t|
-    t.string "category"
     t.string "business"
     t.float "amount"
     t.datetime "created_at", null: false
