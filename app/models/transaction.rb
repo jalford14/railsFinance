6fdcbeefa7c3021c01e0b9f63b5ec7cfa8820c37
Jimmy Alford <jalford14@liberty.edu>
2018-07-21 18:21:22 -0700

    clean up

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,12 @@
 class Transaction < ApplicationRecord
+    belongs_to :category, :optional => true
+
     validates :business, presence: false
     validates :amount, presence: false
     before_save :downcase_fields
-    belongs_to :category
+    
 
    def downcase_fields
-      self.category.downcase!
       if self.business then
         self.business.downcase!
       end
